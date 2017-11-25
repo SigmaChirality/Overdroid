9b79c1b50ac5f4e7930d5e77bb693d0164452568
Chirality <danieltaox@gmail.com>
2017-11-25 17:20:09 -0600

    add jsoup to connect to Overcast servers, basic login screen (no functionality)

---
+++ app/src/main/java/moe/weebsunited/overdroid/LoginActivity.java
@@ -0,0 +1,8 @@
+package moe.weebsunited.overdroid;
+
+
+public class LoginActivity {
+    //setContentView(R.layout.activity_main);
+
+
+}
--- app/src/main/java/moe/weebsunited/overdroid/MainActivity.java
+++ app/src/main/java/moe/weebsunited/overdroid/MainActivity.java
@@ -3,16 +3,32 @@ package moe.weebsunited.overdroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
+    /*final String email = "danieltao@gmail.com";
+    final String password = "/////lolnope";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    private void getWebsite(){
-        Document doc = Jsoup.connect().get();
-    }
+    private Document getWebsite() throws IOException{
+        Document doc = Jsoup.connect("https://overcast.fm/login")
+                .data("email", email)
+                .data("password", password)
+                .userAgent("Chrome")
+                .post();
+        return doc;
+    }*/
 }
 
